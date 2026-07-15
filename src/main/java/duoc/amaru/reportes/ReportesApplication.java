4ae3d5e06251782fda08fd621a04cdc9f815f831
Amaru Burdiles <am.burdiles@duocuc.cl>
2026-07-14 20:21:51 -0400

    Configuración de RestTemplate
    Se movió la conficguración de RestTemplate desde
    Application.java a su propio paquete, "config"

--- src/main/java/duoc/amaru/reportes/ReportesApplication.java
+++ src/main/java/duoc/amaru/reportes/ReportesApplication.java
@@ -2,8 +2,6 @@ package duoc.amaru.reportes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ReportesApplication {
@@ -12,8 +10,4 @@ public class ReportesApplication {
 		SpringApplication.run(ReportesApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
 }
